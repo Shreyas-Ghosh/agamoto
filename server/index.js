b0cb35936c50990f05e3e2e7473a019bd0669b62
Shreyas.Ghosh <f20240826@hyderabad.bits-pilani.ac.in>
2026-06-12 10:49:29 +0530

    fix: explicit cors allow all origins

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import analyzeRoute from './routes/analyze.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*'}));
 app.use(express.json());
 
 app.use('/api', analyzeRoute);
